d102da45baa469690af4e72db64cf56708371106
Roderick@IBM <roderick@karlemstrand.com>
2020-03-31 12:02:28 +0000

    Ex 1 Intro to bash scripting

---
+++ script.sh
@@ -0,0 +1,9 @@
+########################################
+# Codecademy
+# Course: Learn the Command Line
+# Module 5: Bash Scripting
+########################################
+
+#!/bin/bash
+
+echo "Hello Codecademy!"
