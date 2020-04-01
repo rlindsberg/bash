823a1d369f9ccefa3d4b6227b081b52c092efcb5
Roderick@IBM <roderick@karlemstrand.com>
2020-04-01 12:46:46 +0000

    Ex 3 Conditionals

---
+++ 3_Conditionals.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+first_greeting="Nice to meet you!"
+later_greeting="How are you?"
+greeting_occasion=1
+
+if [ $greeting_occasion -lt 2 ]
+then
+  echo $first_greeting
+  ((greeting_occasion=$greeting_occasion+1))
+  echo $greeting_occasion
+else
+  echo $later_greeting
+fi
